6b0e245da51860a6c0701b1bbf8181a712548e96
JUST1984 <yixiezi1994@gmail.com>
2019-10-25 20:00:17 +0800

    CompletableFuture demo

--- basic/src/main/java/priv/just1984/deep/in/java/basic/demo/CompletableFutureDemo.java
+++ basic/src/main/java/priv/just1984/deep/in/java/basic/demo/CompletableFutureDemo.java
@@ -1,5 +1,7 @@
 package priv.just1984.deep.in.java.basic.demo;
 
+import java.util.LinkedList;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -9,20 +11,74 @@ import java.util.concurrent.CompletableFuture;
  */
 public class CompletableFutureDemo {
 
-    public static void main(String[] args) throws InterruptedException {
-        try {
-            CompletableFuture.runAsync(() -> {
-                throw new RuntimeException("JUST1984");
-            }).thenRunAsync(() -> {
-                throw new RuntimeException("JUST1985");
-            }).whenComplete((res, ex) -> {
-                System.out.println("inner catch : " + ex.getMessage());
-            });
-        } catch (Exception e) {
-            System.out.println("outer catch : " + e.getMessage());
-        }
+    private static Random random = new Random();
 
-        Thread.sleep(5000);
+    public static void main(String[] args) throws Exception {
+        /*CompletableFuture<Void> future = CompletableFuture.allOf(CompletableFuture.runAsync(() -> {
+            sleep(2000);
+            System.out.println(1);
+        }), CompletableFuture.runAsync(() -> {
+            sleep(1000);
+            System.out.println(2);
+        }));
+        future.get();
+        System.out.println(3);*/
+
+        /*CompletableFuture[] futureArray = new CompletableFuture[] {CompletableFuture.runAsync(() -> {
+            sleep(2000);
+            System.out.println(1);
+        }), CompletableFuture.runAsync(() -> {
+            sleep(1000);
+            System.out.println(2);
+        })};
+
+        Method andTreeMethod = CompletableFuture.class.getDeclaredMethod("andTree", CompletableFuture[].class, int.class, int.class);
+        andTreeMethod.setAccessible(true);
+        CompletableFuture future = (CompletableFuture) andTreeMethod.invoke(null, futureArray, 0, futureArray.length - 1);
+        future.get();
+        System.out.println(3);*/
+
+        /*CompletableFuture<LinkedList<Object>> future = CompletableFuture.supplyAsync(LinkedList::new).thenCombine(CompletableFuture.supplyAsync(() -> {
+            sleep(2000);
+            System.out.println(1);
+            return 1;
+        }), (list, res) -> {
+            list.add(res);
+            return list;
+        }).thenCombine(CompletableFuture.supplyAsync(() -> {
+            sleep(1000);
+            System.out.println(2);
+            return 2;
+        }), (list, res) -> {
+            list.add(res);
+            return list;
+        }).thenApply(list -> list);
+        LinkedList<Object> res = future.get();
+        System.out.println(res);*/
+
+
+        CompletableFuture<LinkedList<Object>> future = CompletableFuture.supplyAsync(LinkedList::new);
+        for (int i = 0; i < 100; i++) {
+            int j = i;
+            future = future.thenCombine(CompletableFuture.supplyAsync(() -> {
+                System.out.println(j);
+                return j;
+            }), (list, res) -> {
+                list.add(res);
+                return list;
+            });
+        }
+        LinkedList<Object> res = future.get();
+        System.out.println("finish");
+        sleep(10000);
+    }
+
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
